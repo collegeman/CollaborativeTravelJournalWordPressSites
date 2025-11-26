af54c3e080c6d2a38b6673bf39a05511bd3633f7
collegeman <aaron@collegeman.net>
2025-11-26 08:01:39 -0500

    Add WP_DEBUG config

--- config/application.php
+++ config/application.php
@@ -151,6 +151,7 @@ Config::define('CONCATENATE_SCRIPTS', false);
 /**
  * Debugging Settings
  */
+Config::define('WP_DEBUG', env('WP_DEBUG', false));
 Config::define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY', false));
 Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG', false));
 Config::define('SCRIPT_DEBUG', env('SCRIPT_DEBUG', false));
